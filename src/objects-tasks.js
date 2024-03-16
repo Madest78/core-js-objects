8d5b6d8f09d1167237f8640fcc7138feb6691664
Jerzy Matusewicz <vfnectdbx78@gmail.com>
2024-03-16 13:51:44 +0400

    add new solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -111,8 +111,8 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -131,8 +131,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -145,10 +145,27 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
-}
+function makeWord(lettersObject) {
+  const maxPosition = Object.values(lettersObject).reduce((max, positions) => {
+    const maxPos = Math.max(...positions);
+    return maxPos > max ? maxPos : max;
+  }, -1);
 
+  let word = '';
+
+  let i;
+  function addLetters(letter) {
+    if (lettersObject[letter].includes(i)) {
+      word += letter;
+    }
+  }
+
+  for (i = 0; i <= maxPosition; i += 1) {
+    Object.keys(lettersObject).forEach(addLetters);
+  }
+
+  return word;
+}
 /**
  * There is a queue for tickets to a popular movie.
  * The ticket seller sells one ticket at a time strictly in order and give the change.
@@ -163,8 +180,40 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+
+function sellTickets(queue) {
+  let bills25 = 0;
+  let bills50 = 0;
+
+  function checkSale(bill) {
+    if (bill === 25) {
+      bills25 += 1;
+      return true;
+    }
+    if (bill === 50) {
+      if (bills25 === 0) {
+        return false;
+      }
+      bills25 -= 1;
+      bills50 += 1;
+      return true;
+    }
+    if (bill === 100) {
+      if (bills25 > 0 && bills50 > 0) {
+        bills25 -= 1;
+        bills50 -= 1;
+        return true;
+      }
+      if (bills25 >= 3) {
+        bills25 -= 3;
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+
+  return queue.every(checkSale);
 }
 
 /**
@@ -180,9 +229,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
 }
+
+Rectangle.prototype.getArea = function () {
+  return this.width * this.height;
+};
 
 /**
  * Returns the JSON representation of specified object
